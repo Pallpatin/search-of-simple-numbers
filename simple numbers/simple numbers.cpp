25c7bb67bdd2dc7020a36bc2660f45d93fbfb1ac
Paul Lopatin <lopatinpavel96@gmail.com>
2023-05-08 00:47:49 +0300

    read file work

--- simple numbers/simple numbers.cpp
+++ simple numbers/simple numbers.cpp
@@ -8,27 +8,49 @@ unsigned long* readfile(string path) {
     ifstream in(path);
     if (in.is_open()) {
         unsigned long siz;
-        in.seekg(16,ios_base::beg);
-        in >> siz;
-        in.seekg(17+size(to_string(siz)), ios_base::beg);
-        string str;
-        unsigned long* buf = new unsigned long[siz];
-        while (!in.eof()) {
-            in >> str;
-            cout << str << endl;
+        string num = "";
+        char ch;
+        in.seekg(-3,ios_base::end);
+        in.get(ch);
+        while (ch != ';') {
+            in.seekg(-2,ios_base::cur);
+            in.get(ch);
         }
+        while (ch != '\n') {
+            in.seekg(-2, ios_base::cur);
+            in.get(ch);
+            num = ch + num;
+        }
+        siz = stoi(num);
+        unsigned long* buf = new unsigned long[siz];
+        in.seekg(18, ios_base::beg);
+        
+        for (int i = 0; i < siz; i++) {
+            while (ch != ';') {
+                in.get(ch);
+            }
+            getline(in, num);
+            buf[i] = stoi(num);
+            in.get(ch);
+        }
+        in.close();
+        return buf;
     }
     else {
         in.close();
         ofstream out(path);
-        out << "pozition;number;0\n";
+        out << "pozition;number\n";
         out.close();
         return new unsigned long[0]; 
     }
 }
 
+
+
 int main()
 {
     string path = "simplenums.csv";
-    readfile(path);
+    unsigned long* arr = readfile(path);
+    int size = _msize(arr) / sizeof(arr[0]);
+    for (int i = 0; i < size; i++)cout << arr[i] << endl;
 }
